9de4e909d56b1e258012f213a8ffd4300d8fea98
Glebanister <glebmar2001@gmail.com>
2020-10-12 23:24:24 +0300

    Restore parsnip from ashes of nuclear fire

--- extern/Parsnip_0.23/main.cpp
+++ extern/Parsnip_0.23/main.cpp
@@ -10,28 +10,29 @@ using namespace Parsnip;
 
 double multiply(double x, double y)
 {
-	return x * y;
+	return x*y;
 }
 double add(double x, double y)
 {
-	return x + y;
+	return x+y;
 }
 double subtract(double x, double y)
 {
-	return x - y;
+	return x-y;
 }
 double divide(double x, double y)
 {
-	return x / y;
+	return x/y;
 }
 
-int main(int argc, char *const argv[])
-{
+
+int main (int argc, char * const argv[]) 
+{	
 	typedef Parser<string, double>::type NumParser;
 
 	NumParser op_self = lazy<string, double>();
-
-	NumParser term = real | skip_ch('(') >> op_self >> skip_ch(')');
+	
+	NumParser term =  real |  skip_ch('(') >> op_self >> skip_ch(')'); 
 
 	NumParser ops = op_table(term)
 						->infix_left("+", 10, add)
@@ -41,36 +42,35 @@ int main(int argc, char *const argv[])
 
 	setLazy(op_self, ops);
 
-	NumParser expr = ops | term;
+	NumParser expr =  ops | term;
 
 	std::string input;
 	ParseResult<double> result;
+	
 
 	while (true)
 	{
 		std::cout << "> ";
-		std::getline(std::cin, input);
+	    std::getline(std::cin, input);
 
-		if (input == "exit")
-			break;
-
-		result = parse(input, expr);
+		if (input == "exit") break;		
+		
+		result =  parse(input, expr);
 
 		if (result.parse_finished())
 		{
-			cout << endl
-				 << result.data() << endl;
+			cout << endl << result.data() << endl;
 		}
-		else if (result.input_consumed())
+		else if (result.input_consumed()) 
 		{
 			cout << "Unexpected end of input string " << endl;
 		}
 		else
 		{
 			std::string::size_type pos = result.parse_position();
-			cout << "Unexpected character '" << input[pos] << "' at position " << pos << endl;
+			cout << "Unexpected character '" << input[pos] << "' at position " << pos  << endl;
 		}
 	}
-
-	return 0;
+	
+    return 0;
 }
